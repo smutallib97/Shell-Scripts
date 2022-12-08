0e1728b67903e1e057dab0c9a423964070f9ec3b
smutallib97 <smutallib97@gmail.com>
2022-12-08 11:26:31 +0530

    Shell Basics

---
+++ Basics/variables.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+echo "System Variables"
+
+echo $BASH #bash directory
+echo $BASH_VERSION  #bash version
+echo $HOME #home directory
+echo $PWD  #present working directory
+
+echo "User Defined Variables"
+
+name=Suraj
+echo My friend name is $name
+empid=101
+echo Employee ID is $empid
